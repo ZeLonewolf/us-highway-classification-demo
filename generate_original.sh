d3120d42fdc7499c2aa3618a2f1035922766797b
Brian Sperlongano <zelonewolf@gmail.com>
2021-05-18 02:25:03 +0000

    Update scripts to generate new functional highway files

--- generate_original.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-./download.sh
-
-osmium tags-filter -O -o tmp/highways-ne.pbf download/us-northeast-latest.osm.pbf w/highway=motorway,trunk,primary,motorway_link,trunk_link,primary_link
-osmium tags-filter -O -o tmp/highways-nb.pbf download/new-brunswick-latest.osm.pbf w/highway=motorway,trunk,primary,motorway_link,trunk_link,primary_link
-osmium tags-filter -O -o tmp/highways-qc.pbf download/quebec-latest.osm.pbf w/highway=motorway,trunk,primary,motorway_link,trunk_link,primary_link
-
-osmium merge -O -o northeast_original.osm.pbf tmp/highways-ne.pbf tmp/highways-nb.pbf tmp/highways-qc.pbf
